6a4cee1dbd150cc3b907bbf283c1d354f5ab912a
JaysanJaychiev <jaysan.jaychiev.11@gmail.com>
2022-01-27 13:55:14 +0600

    update settings

--- SimpleUser/models.py
+++ SimpleUser/models.py
@@ -1,12 +1,12 @@
 from datetime import date
 from django.db import models
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+# borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-@property
-def is_overdue(self):
-    if self.duo_back and date.today() > self.due_back:
-        return True
-    return False
+# @property
+# def is_overdue(self):
+#     if self.duo_back and date.today() > self.due_back:
+#         return True
+#     return False
--- UserAuth/settings/production.py
+++ UserAuth/settings/production.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
