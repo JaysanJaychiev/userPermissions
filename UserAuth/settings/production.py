d56f5600f887f0a3d4021dd151f7100a74d2bc5a
JaysanJaychiev <jaysan.jaychiev.11@gmail.com>
2022-01-27 20:19:38 +0600

    update settings

--- UserAuth/settings/production.py
+++ UserAuth/settings/production.py
@@ -106,16 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-SERVER_EMAIL = EMAIL_HOST_USER
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 LANGUAGE_CODE = 'ru'
